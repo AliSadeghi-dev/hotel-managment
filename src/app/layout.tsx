1eee31ba79c2a10bba31c6d7eba8d19a47b6ceff
AliSadeghi <alisadghi228@gmail.com>
2024-07-19 12:24:44 +0330

    feat : config header and footer and add light and dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import {ThemeProvider} from "@/components/ThemeProvider/ThemeProvider";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,8 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header />
-        <main className="font-normal">{children}</main>
+        <ThemeProvider>
+          <Header />
+          <main className="font-normal">{children}</main>
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
