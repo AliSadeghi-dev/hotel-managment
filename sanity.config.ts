81e650bd9c61dedab95b6b102ccba808ff3789e7
AliSadeghi <alisadghi228@gmail.com>
2024-07-21 09:30:36 +0330

    feat : add all schemas in sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: "default",
   title: "hotel-managment",
 
-  projectId: "2jx0igcc",
-  dataset: "hotel-managment",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
 
   basePath: "/studio",
 
